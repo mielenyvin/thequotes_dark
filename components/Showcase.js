9a13147378485a7ea0e8557de2685fa2a0469229
mielenyvin <80074568+mielenyvin@users.noreply.github.com>
2022-01-16 15:40:03 +0100

    design

--- components/Showcase.js
+++ components/Showcase.js
@@ -6,7 +6,7 @@ export default function Showcase() {
 {/* id="bgstyle" bg-repeat  */}
 
 <div className="w-full flex flex-row justify-center">
-<h2 className=" bg-transparent bg-qblue bg-opacity-100 rounded-3xl px-4 py-2 text-white">The truth in the middle</h2></div>
+<h2 className=" bg-transparent bg-white bg-opacity-100 rounded-3xl px-4 py-2 text-black">The truth in the middle</h2></div>
 </div>
           )
 }
