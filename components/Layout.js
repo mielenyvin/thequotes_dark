4ce115c0ac17290a6c35c956679195f99e6122c5
mielenyvin <80074568+mielenyvin@users.noreply.github.com>
2022-01-18 12:50:55 +0100

    design updates

--- components/Layout.js
+++ components/Layout.js
@@ -31,37 +31,28 @@ export default function Layout({title, keywords, desciprtion, children, categori
 </div>
 </div>
 
-<div className='pb-5 sm:pb-5'></div>
+<div className='pb-0 sm:pb-5'></div>
 
 {/* {(router.pathname === '/' || router.pathname === '/about' || router.pathname === '/news/category' || router.pathname === '/news/search') && <Showcase />} */}
             
-{/* <div className='flex-row flex gap-5 container'>
-<div className='hidden sm:block text-white'>sm</div>
-<div className='hidden md:block text-white'>md</div>
-<div className='hidden lg:block text-white'>lg</div>
-<div className='hidden xl:block text-white'>xl</div>
-<div className='hidden 2xl:block text-white '>2xl</div>
-</div> */}
-
-{(router.pathname === '/about' || router.pathname === '/news/category' || router.pathname === '/news/search' ) && <div className='sm:mx-6 sm:h-screen'> 
+{(router.pathname === '/about' || router.pathname === '/news/category' || router.pathname === '/news/search' ) && <div className='sm:mx-6 h-screen'> 
 <div className='bg-qlitegray sm:rounded-2xl container mx-auto flex flex-wrap p-5 flex-col '>
-<main className='mb-auto container mx-auto sm:px-5'><div className='mt-5'></div>
+<main className='mb-auto container mx-auto px-5'><div className='mt-5'></div>
 <div className=''>{children}</div>
 <div className='mt-5'></div>
 </main>
 </div>
 </div>}
 
-{(router.pathname === '/' || (router.pathname != '/about' && router.pathname != '/news/category' && router.pathname != '/news/search')  ) && 
-<div className=' sm:mx-6'>
-<div className='bg-qlitegray sm:rounded-2xl container mx-auto flex flex-wrap pt-5 p-5 flex-col '>
-<main className='mb-auto container mx-auto sm:px-5'>
-<div className='sm:mt-5'></div><div className=''>{children}</div>
+{(router.pathname === '/'   ) && 
+<div className='sm:mx-6'>
+<div className='bg-qlitegray sm:rounded-2xl container mx-auto flex flex-wrap p-5 flex-col '>
+<main className='mb-auto container mx-auto px-5'>
+<div className='mt-5'></div><div className=''>{children}</div>
 <div className='mt-5'></div></main></div></div>}
        
-       
 <div className='py-5'>
-{(router.pathname != '/about' ) && <Footer /> }
+<Footer />
 </div>
 </div>
     )
