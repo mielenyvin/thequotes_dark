f0ad4fe69843706d7988a6f8cee27350d150fdba
mielenyvin <80074568+mielenyvin@users.noreply.github.com>
2022-01-18 12:00:15 +0100

    design updates

--- components/CategoryList.js
+++ components/CategoryList.js
@@ -5,12 +5,12 @@ export default function CategoryList({ categories }) {
   return (
     
     <div id="main" className="lg:grid-cols-6 md:grid-cols-3 grid grid-cols-3 h-18 lg:h-8 m-5 gap-1 justify-evenly text-center divide-metal  lg:divide-x"> 
-     <Link href="/news/category?term=politics"><div className="py-1 cursor-pointer hover:text-qorange">Politics</div></Link>
-     <Link href="/news/category?term=business"><div className="py-1 cursor-pointer hover:text-qorange">Business</div></Link>
-     <Link href="/news/category?term=technology"><div className="py-1 cursor-pointer hover:text-qorange">Technology</div></Link>
-     <Link href="/news/category?term=health"><div className="py-1 cursor-pointer hover:text-qorange">Health</div></Link>
-     <Link href="/news/category?term=sport"><div className="py-1 cursor-pointer hover:text-qorange">Sport</div></Link>
-     <Link href="/news/category?term=culture"><div className="py-1 cursor-pointer hover:text-qorange">Culture</div></Link>
+     <Link href="/news/category?term=politics"><div className="h-8 py-1 cursor-pointer hover:text-qorange">Politics</div></Link>
+     <Link href="/news/category?term=business"><div className="h-8 py-1 cursor-pointer hover:text-qorange">Business</div></Link>
+     <Link href="/news/category?term=technology"><div className="h-8 py-1 cursor-pointer hover:text-qorange">Technology</div></Link>
+     <Link href="/news/category?term=health"><div className="h-8 py-1 cursor-pointer hover:text-qorange">Health</div></Link>
+     <Link href="/news/category?term=sport"><div className="h-8 py-1 cursor-pointer hover:text-qorange">Sport</div></Link>
+     <Link href="/news/category?term=culture"><div className="h-8 py-1 cursor-pointer hover:text-qorange">Culture</div></Link>
     </div> 
   )
 
